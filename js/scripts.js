7250e09dd249a8ebb0aea0690540bc5addb6be69
Joanna Anderson and Sandro Alvarez <sandro@>
2016-08-09 15:06:25 -0700

    add images and styling to the carousel

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,22 @@ $(function(){
     $("h2").toggle();
     $("p").toggle();
   });
+  $(".active").click(function() {
+
+  })
   $("p").click(function(){
     $("h2").toggle();
     $("p").toggle();
   });
   $(".carousel").carousel({
     interval: null
+  });
+  $(".carousel-control").click(function(){
+    $("h2").show();
+    $("p").hide();
+  });
+  $(".carousel-indicators").click(function(){
+    $("h2").show();
+    $("p").hide();
   });
 });
